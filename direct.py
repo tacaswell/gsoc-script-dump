d97e25d73bb7b2e17e6082782f2528dcf4177157
patniharshit <patniharshit@gmail.com>
2017-06-12 17:47:05 +0530

    Plotting 2d rgba array with imshow

--- direct.py
+++ direct.py
@@ -19,7 +19,7 @@ bivariate_cm = [np.array([my_cmap[i], ]*alpha_size) for i in range(cmap_size)]
 for arr in bivariate_cm:
     arr[:, -1] = alpha_val
 
-nrows = 128
+nrows = 256
 x = y = np.linspace(-5, 5, nrows)
 X, Y = np.meshgrid(x, y)
 R = np.sqrt(X**2 + Y**2)
@@ -36,45 +36,35 @@ my_cmap = ListedColormap(bivariate_cm[0])
 normz = mpl.colors.Normalize(Z.min(), Z.max())
 normgx = mpl.colors.Normalize(gx.min(), gx.max())
 
-temp = Z
 gx = np.array(normgx(gx)*255, dtype='int64')
+gy = np.array(normgx(gy)*255, dtype='int64')
 Z = np.array(normz(Z)*255, dtype='int64')
 
-print Z.shape
 colarr = np.ones(shape=Z.shape + (4,))
-print colarr[0][0]
 
-
-def plot_color_gradients(nrows):
-    # fig, axes = plt.subplots(nrows=nrows)
-
-    # fig.subplots_adjust(top=0.999, bottom=0.001, left=0.001, right=0.999, hspace=0, wspace=0)
-
+def plot_color_gradients(nrows, G):
     jdx = 0
-    # for ax, zarr, gxarr in zip(axes, Z, gx):
-    for zarr, gxarr in zip(Z, gx):
+    for zarr, gxarr in zip(Z, gy):
         idx = 0
         for zele, gxele in zip(zarr, gxarr):
             # print zele, gxele
-            gxele = 255
-            colarr[jdx][idx] = bivariate_cm[zele][255]
+            if G < 0:
+                colarr[jdx][idx] = bivariate_cm[zele][gxele]
+            else:
+                colarr[jdx][idx] = bivariate_cm[zele][G]
             # print bivariate_cm[zele][gxele]
             idx = idx+1
         jdx = jdx+1
-        # my_cmap = ListedColormap(colarr)
-
-        # gradient = zparr
-        # gradient = np.vstack((gradient, gradient))
-        # ax.imshow(gradient, aspect='auto', cmap = my_cmap)
-        # ax.imshow(gradient, aspect='auto')
-
-    # Turn off *all* ticks & spines, not just the ones with colormaps.
-    # for ax in axes:
-    #    ax.set_axis_off()
 
 
-plot_color_gradients(nrows)
+fig, axes = plt.subplots(ncols=3)
 
-fig, axes = plt.subplots(nrows=1)
-axes.imshow(colarr)
+plot_color_gradients(nrows, 255)
+axes[0].imshow(colarr)
+
+plot_color_gradients(nrows, -1)
+axes[1].imshow(colarr)
+
+axes[2].imshow(gy)
+
 plt.show()
