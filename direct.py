4e60e20fa566e2e213261e77ac2a150ac4a02c4f
patniharshit <patniharshit@gmail.com>
2017-06-12 17:33:00 +0530

    Directly pass 2d rgba array to imshow

--- direct.py
+++ direct.py
@@ -4,7 +4,7 @@ from matplotlib.colors import ListedColormap
 import numpy as np
 from pylab import rcParams
 
-# rcParams['figure.figsize'] = 4, 4
+#rcParams['figure.figsize'] = 4, 4
 # Choose colormap
 cmap = plt.cm.viridis
 
@@ -19,7 +19,7 @@ bivariate_cm = [np.array([my_cmap[i], ]*alpha_size) for i in range(cmap_size)]
 for arr in bivariate_cm:
     arr[:, -1] = alpha_val
 
-nrows = 16
+nrows = 128
 x = y = np.linspace(-5, 5, nrows)
 X, Y = np.meshgrid(x, y)
 R = np.sqrt(X**2 + Y**2)
@@ -30,45 +30,51 @@ gx, gy = np.gradient(Z, dx, dx)
 
 my_cmap = ListedColormap(bivariate_cm[0])
 
+# fig, axes = plt.subplots(nrows=1)
+# axes.imshow(Z, origin='lower')
+
 normz = mpl.colors.Normalize(Z.min(), Z.max())
 normgx = mpl.colors.Normalize(gx.min(), gx.max())
 
 temp = Z
 gx = np.array(normgx(gx)*255, dtype='int64')
 Z = np.array(normz(Z)*255, dtype='int64')
-Z = Z[0:70]
-# fig, axes = plt.subplots(nrows=1)
-# axes.imshow(Z)
+
+print Z.shape
+colarr = np.ones(shape=Z.shape + (4,))
+print colarr[0][0]
+
 
 def plot_color_gradients(nrows):
-    # return
-    colarr = np.empty(shape=(1, 3) + (4,))
+    # fig, axes = plt.subplots(nrows=nrows)
 
-    fig, axes = plt.subplots(nrows=nrows)
+    # fig.subplots_adjust(top=0.999, bottom=0.001, left=0.001, right=0.999, hspace=0, wspace=0)
 
-    fig.subplots_adjust(top=0.999, bottom=0.001, left=0.001, right=0.999, hspace = 0, wspace = 0)
-
-    for ax, zarr, gxarr, temparr in zip(axes, Z, gx, temp):
+    jdx = 0
+    # for ax, zarr, gxarr in zip(axes, Z, gx):
+    for zarr, gxarr in zip(Z, gx):
         idx = 0
         for zele, gxele in zip(zarr, gxarr):
-            print zele, gxele
-
-            lgxele = 255
-            colarr[idx] = bivariate_cm[-zele][gxele]
-            print colarr[idx]
+            # print zele, gxele
+            gxele = 255
+            colarr[jdx][idx] = bivariate_cm[zele][255]
+            # print bivariate_cm[zele][gxele]
             idx = idx+1
+        jdx = jdx+1
+        # my_cmap = ListedColormap(colarr)
 
-        my_cmap = ListedColormap(colarr)
+        # gradient = zparr
+        # gradient = np.vstack((gradient, gradient))
+        # ax.imshow(gradient, aspect='auto', cmap = my_cmap)
+        # ax.imshow(gradient, aspect='auto')
 
-        gradient = zarr
-        #gradient = np.linspace(0,1,105)
-        gradient = np.vstack((gradient, gradient))
-        ax.imshow(gradient, aspect='auto', cmap=my_cmap)
-        #break
     # Turn off *all* ticks & spines, not just the ones with colormaps.
-    for ax in axes:
-        ax.set_axis_off()
+    # for ax in axes:
+    #    ax.set_axis_off()
+
 
 plot_color_gradients(nrows)
 
+fig, axes = plt.subplots(nrows=1)
+axes.imshow(colarr)
 plt.show()
